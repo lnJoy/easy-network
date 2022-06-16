802e3b536494857713b22712a1b93a1b6348efab
lnJoy <lnjoy3306@gmail.com>
2022-06-16 22:55:30 +0900

    fix: Http Status Code를 Created에서 OK로 변경

--- server/vlsm/vlsm.controller.ts
+++ server/vlsm/vlsm.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { CreateSubnetDto } from './dto/create-subnet.dto';
 import { VLSM } from './models/vlsm.model';
 import { VLSMService } from './vlsm.service';
@@ -8,6 +8,7 @@ export class VLSMController {
   constructor(private readonly vlsmService: VLSMService) {}
 
   @Post()
+  @HttpCode(200)
   async create(@Body() createSubnetDto: CreateSubnetDto): Promise<VLSM[]> {
     const vlsm_result: VLSM[] | undefined = this.vlsmService.compute(createSubnetDto);
     if (vlsm_result === null) throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
