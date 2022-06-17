784e436b543ee0728c4a3ce967aaf2ba0ecd762f
lnJoy <20sunrin007@sunrint.hs.kr>
2022-06-17 12:28:48 +0900

    feat: 서브넷팅 결과 값을 받아올 모델 추가

--- server/subnet/models/subnet.model.ts
+++ server/subnet/models/subnet.model.ts
@@ -1,6 +1,13 @@
+import { VLSM } from "./vlsm.model";
 
 export class Subnet {
   readonly name: string;
   
   readonly needed_size: number;
 };
+
+export class ResultSubnet {
+  readonly network_id: string;
+
+  readonly subnets: VLSM[]
+}
